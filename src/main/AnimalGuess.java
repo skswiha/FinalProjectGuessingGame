3319e824b95113a0313360d2fdbbd67b992179a6
Sarah Swihart <skswiha@gmail.com>
2019-04-25 15:01:33 -0400

    Added AnimalTree.txt and worked on main in AnimalGuess, wrote FollowPath in DecisionTree

--- src/main/AnimalGuess.java
+++ src/main/AnimalGuess.java
@@ -1,11 +1,22 @@
 package main;
 
+import java.io.*;
+
 public class AnimalGuess {
 	public static void main (String [] args) {
+		BufferedReader in;
+		try {
+			in = new BufferedReader(new FileReader("AnimalTree.txt"));
+		} catch (FileNotFoundException e) {
+			System.out.println("File Not Found");
+		}
 		boolean playing = true;
+		System.out.println("Think of an animal.\nI'll try to guess it.");
 		while (playing == true){
-			
 		}
 		
+	}
+	
+	public void classify() {
 	}
 }
--- src/main/DecisionTree.java
+++ src/main/DecisionTree.java
@@ -1,8 +1,28 @@
 package main;
 
 public class DecisionTree extends BinaryTree<String> {
-
-	public DecisionTree(BinaryTree<String> tree) {
+	
+	public DecisionTree(BinaryTree<String> tree, String file) {
 		super(tree);
+	}
+	
+	public String followPath(String text){
+		boolean inPath = true;
+		int location = 0;
+		BinaryTree<String> nowAt = this;
+		while(inPath == true) {
+			if(text.charAt(location) == 'Y') {
+				nowAt = this.getLeft();
+			}
+			else if(text.charAt(location) == 'N') {
+				nowAt = this.getRight();
+			}
+			else {
+				inPath = false;
+			}
+			location++;
+		}
+		
+		return nowAt.getData();
 	}
 }
