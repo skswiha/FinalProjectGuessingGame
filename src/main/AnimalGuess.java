36cba04125b54dc2e1679e1e855a3db68517c9a8
Sarah Swihart <skswiha@gmail.com>
2019-04-27 17:07:28 -0400

    Worked on DecisionTree and AnimalGuess, almost done with it but can't figure out how to fill the tree.

--- src/main/AnimalGuess.java
+++ src/main/AnimalGuess.java
@@ -1,18 +1,37 @@
 package main;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class AnimalGuess {
 	public static void main (String [] args) {
-		BufferedReader in;
+		Scanner stan = new Scanner(System.in);
+		String answer;
+		String currentPath = "";
+		BufferedReader in = null;
+		DecisionTree tree = null;
 		try {
 			in = new BufferedReader(new FileReader("AnimalTree.txt"));
-		} catch (FileNotFoundException e) {
-			System.out.println("File Not Found");
+			tree = new DecisionTree(in.readLine());
+			tree.fillTree(in);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		boolean playing = true;
 		System.out.println("Think of an animal.\nI'll try to guess it.");
 		while (playing == true){
+			System.out.println(tree.followPath(currentPath));
+			answer = stan.nextLine().toLowerCase();
+			if (answer.contains("yes")) {
+				currentPath += "Y";
+			}
+			else if (answer.contains("no")) {
+				currentPath += "N";
+			}
+			else {
+				System.out.println("Please respond with yes or no.");
+			}
+			System.out.println(currentPath);
 		}
 		
 	}
--- src/main/DecisionTree.java
+++ src/main/DecisionTree.java
@@ -1,9 +1,11 @@
 package main;
 
+import java.io.*;
+
 public class DecisionTree extends BinaryTree<String> {
 	
-	public DecisionTree(BinaryTree<String> tree, String file) {
-		super(tree);
+	public DecisionTree(String data) {
+		super(data);
 	}
 	
 	public String followPath(String text){
@@ -11,10 +13,10 @@ public class DecisionTree extends BinaryTree<String> {
 		int location = 0;
 		BinaryTree<String> nowAt = this;
 		while(inPath == true) {
-			if(text.charAt(location) == 'Y') {
+			if(location < text.length() && text.charAt(location) == 'Y') {
 				nowAt = this.getLeft();
 			}
-			else if(text.charAt(location) == 'N') {
+			else if(location < text.length() && text.charAt(location) == 'N') {
 				nowAt = this.getRight();
 			}
 			else {
@@ -22,7 +24,31 @@ public class DecisionTree extends BinaryTree<String> {
 			}
 			location++;
 		}
-		
-		return nowAt.getData();
+		if (nowAt != null){
+			return nowAt.getData();
+		}
+		else {
+			return "I got it wrong. Please help me to learn.";
+		}
+	}
+	
+	public void fillTree(BufferedReader in) throws IOException {
+		String line = "";
+		BinaryTree<String> current = this;
+		int countLeft = 0;
+		int countRight = 0;
+		for (int i = 0; i < 12; ++i) {
+			line = in.readLine();
+			if (line != null && line.charAt(countLeft) == 'Y') {
+				current.setLeft(new BinaryTree<String>(line));
+				countLeft++;
+			}
+			else if (line != null && line.charAt(countRight) == 'N') {
+				current.setRight(new BinaryTree<String>(line));
+				countRight++;
+				current = current.getLeft();
+			}
+			
+		}
 	}
 }
